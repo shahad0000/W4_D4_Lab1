1dca47ba1da7066312ffb07a988b641028379418
shahad0000 <81360310+shahad0000@users.noreply.github.com>
2025-05-07 10:44:49 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+
+// 1
+let h1 = document.getElementsByClassName('first')[0];
+h1.style.backgroundColor = "black";
+h1.style.color = "white";
+
+
+// 2
+let pElement = document.getElementById('req-2');
+pElement.style.fontWeight = "bold";
+
+// 3
+let imgElement = document.getElementsByTagName(img).src = "/image.png";
+
+// 4
+let ul = document.getElementById("list");
+let item1 = document.createElement('li').innerText = "one";
+ul.appendChild = item1;
+
+
